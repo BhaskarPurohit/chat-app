58ed6fa5e1b2dda950a284ed3178ceeb2630dd77
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-16 23:40:00 +0530

    new

--- src/component/Join/Join.js
+++ src/component/Join/Join.js
@@ -8,6 +8,8 @@ const Join = () => {
         <div className="JoinContainer">
             <img src={logo} alt="" />
             <h1>C Chat</h1>
+            <input type="text"  id="joinInput" />
+            <button className='joinbtn'>Log In</button>
         </div>
     </div>
   )
